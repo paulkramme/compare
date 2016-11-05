39fec4cde702ff32464aaf3a52b9c599e78bd381
Paul Kramme <pjkramme@gmail.com>
2016-11-05 19:08:48 +0100

    Add main function

--- compare.py
+++ compare.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
-file1 = input("Input your first checksum: ")
-file2 = input("Input your second checksum: ")
+def main():
+    file1 = input("Input your first checksum: ")
+    file2 = input("Input your second checksum: ")
 
-if file1 == file2:
-    print("The files are identical.")
-else:
-    print("The file are NOT identical.")
+    if file1 == file2:
+        print("The files are identical.")
+    else:
+        print("The file are NOT identical.")
+
+
+if __name__ == __name__:
+    main()
