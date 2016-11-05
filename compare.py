051107c81f1706904b7a2dd5b50c2902f426c58e
Paul Kramme <pjkramme@gmail.com>
2016-11-05 19:04:59 +0100

    Add baseline

---
+++ compare.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+file1 = input("Input your first checksum: ")
+file2 = input("Input your second checksum: ")
+
+if file1 == file2:
+    print("The files are identical.")
+else:
+    print("The file are NOT identical.")
